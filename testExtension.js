511905309bbac390c0eb2f94392f5437f380d321
lucinder <81818595+lucinder@users.noreply.github.com>
2024-04-28 10:27:42 -0700

    making html look nicer

--- testExtension.js
+++ testExtension.js
@@ -1,3 +1,72 @@
+const exampleVulns = [
+    {"name":"CWE-23: Relative Path Traversal", // 1
+     "link": "https://cwe.mitre.org/data/definitions/23.html",
+     "lineNo":11,
+     "desc" : "The product uses external input to construct a pathname that should be within a restricted directory, but it does not properly neutralize sequences such as \"..\" that can resolve to a location that is outside of that directory."},
+    {"name":"CWE-261: Weak Encoding for Password", // 2
+     "link": "https://cwe.mitre.org/data/definitions/261.html",
+     "lineNo":25,
+     "desc" : "Obscuring a password with a trivial encoding does not protect the password."},
+    {"name":"CWE-523: Unprotected Transport of Credentials",
+     "link": "https://cwe.mitre.org/data/definitions/523.html",
+     "lineNo":30, // 3
+     "desc" : "Login pages do not use adequate measures to protect the user name and password while they are in transit from the client to the server."},
+    {"name":"CWE-20: Improper Input Validation",
+     "link": "https://cwe.mitre.org/data/definitions/20.html",
+     "lineNo":9, // 4
+     "desc" : "The product receives input or data, but it does not validate or incorrectly validates that the input has the properties that are required to process the data safely and correctly."},
+    {"name":"CWE-89: Improper Neutralization of Special Elements used in an SQL Command ('SQL Injection')",
+     "link": "https://cwe.mitre.org/data/definitions/89.html",
+     "lineNo":87, // 5
+     "desc" : "The product constructs all or part of an SQL command using externally-influenced input from an upstream component, but it does not neutralize or incorrectly neutralizes special elements that could modify the intended SQL command when it is sent to a downstream component. "},
+    {"name":"CWE-94: Improper Control of Generation of Code ('Code Injection')",
+     "link": "https://cwe.mitre.org/data/definitions/94.html",
+     "lineNo":89, // 6
+     "desc" : "The product constructs all or part of a code segment using externally-influenced input from an upstream component, but it does not neutralize or incorrectly neutralizes special elements that could modify the syntax or behavior of the intended code segment. "},
+    {"name":"CWE-260: Password in Configuration File",
+     "link": "https://cwe.mitre.org/data/definitions/260.html",
+     "lineNo":2, // 7
+     "desc" : "The product stores a password in a configuration file that might be accessible to actors who do not know the password."},
+    {"name":"CWE-502: Deserialization of Untrusted Data",
+     "link": "https://cwe.mitre.org/data/definitions/502.html",
+     "lineNo":100, // 8
+     "desc" : "The product deserializes untrusted data without sufficiently verifying that the resulting data will be valid. "},
+    {"name":"CWE-565: Reliance on Cookies without Validation and Integrity Checking",
+     "link": "https://cwe.mitre.org/data/definitions/565.html",
+     "lineNo":120, // 9
+     "desc" : "The product relies on the existence or values of cookies when performing security-critical operations, but it does not properly ensure that the setting is valid for the associated user."},
+    {"name":"CWE-830: Inclusion of Web Functionality from an Untrusted Source",
+     "link": "https://cwe.mitre.org/data/definitions/830.html",
+     "lineNo":132, // 10
+     "desc" : "The product includes web functionality (such as a web widget) from another domain, which causes it to operate within the domain of the product, potentially granting total access and control of the product to the untrusted source. "}
+  ];
+  const gradeBgs = {
+    'A' : "#d5f5e3",
+    'B' : "#fcf3cf",
+    'C' : "#fdebd0",
+    'D' : "#fae5d3",
+    'F' : "#fadbd8" };
+  const gradeTexts = {
+    'A' : "#145a32",
+    'B' : "#7d6608",
+    'C' : "#7e5109",
+    'D' : "#784212",
+    'F' : "#641e16",
+  }
+  const gradeBadges = {
+    'A' : "#2ecc71",
+    'B' : "#f4d03f",
+    'C' : "#e67e22",
+    'D' : "#dc7633",
+    'F' : "#e74c3c",
+  }
+  const gradeThresholds = {
+    7: 'D',
+    5: 'C',
+    3: 'B',
+    1: 'A' };
+
+
 /**
 
 import { createRequire } from 'module';
@@ -8,6 +77,7 @@ const fs = require('fs')
 //Requre child process for running npm
 const {execSync } = require('child_process')
 
+**/ 
 //function to create a grader object that can be used by the extension
 let CodeGrader = function (codeContent, codeType) {
     //create object
@@ -32,41 +102,73 @@ let CodeGrader = function (codeContent, codeType) {
         switch (codeGrader.type) {
             case "JavaScript":
                 //run eslint for js code
-                execSync('nps eslint download.txt')
+                console.log("This is where we would grade javascript!");
+                // execSync('nps eslint download.txt')
             // if no matching code type was found log as unsupported code language
             default:
                 console.log("Usupported code type detected")
         }
-
+        return "Example output";
     };
 
     return codeGrader;
 }
 
-//create a new grader 
-let grader = CodeGrader("", "JavaScript")
+function getExampleVulnResults(vulnCount){ // make a list with our example vulns
+    vulnList = [];
+    for(let i = 0; i < vulnCount; i++){
+      vulnList.push(exampleVulns[i]);
+    } // push vulncount vulns to output
+    return vulnList;
+  }
 
-//save the file
-grader.saveFile
-
-//run the linter
-grader.grade
-
-**/
-
-const getAllChildren = (htmlElement) => { // get all children of a node
-    if (htmlElement.children.length === 0) return [htmlElement];
-  
-    let allChildElements = [];
-  
-    for (let i = 0; i < htmlElement.children.length; i++) {
-      let children = getAllChildren(htmlElement.children[i]);
-      if (children) allChildElements.push(...children);
+  function displayGrade(element, vulnTotal){ // build grade output
+    let score = Number(vulnTotal); // convert to int
+    if(score > 10){ score = 10; } // limit to max 10
+    vulns = getExampleVulnResults(score); // grab some example vulns
+    
+    let badge = document.createElement("div");
+    badge.className = "flex flex-col";
+    element.appendChild(badge);
+    badge.style = " \
+        min-width: 50px; \
+        height: 50px; \
+        border-radius: 50%; \
+        margin-right: 10px; \
+        margin-bottom: 10px; \
+        text-align: center; \
+        line-height: 50px; \
+        font-size: 24px; \
+        color: white; \
+    ";
+    let textbox = document.createElement("div");
+    textbox.className = "flex flex-col";
+    element.appendChild(textbox);
+    textbox.style = " \
+        font-size: 16px; \
+    ";
+    // console.log(score);
+    letterGrade = 'F';
+    for (var grade in gradeThresholds){ // find our letter grade
+      if (score < grade){
+        letterGrade = gradeThresholds[grade];
+        break;
+      }
     }
-    allChildElements.push(htmlElement);
-  
-    return allChildElements;
-  };
+    // grab colors for this grade
+    element.style.background = gradeBgs[letterGrade];
+    badge.style.background = gradeBadges[letterGrade];
+    element.style.color = gradeTexts[letterGrade];
+    // build output
+    badge.innerHTML = letterGrade;
+    output = "<b style='font-size:20px;'>" + score + " vulnerabilities found!</b><br><br>";
+    for (vuln in vulns){
+      vuln = vulns[vuln];
+      output += "<a href='" + vuln["link"] + "' style='color:blue;'>" + vuln["name"] + "</a>. (line " + vuln["lineNo"] + ")<br><t>" + vuln["desc"] + "<br><br>";
+    }
+    textbox.innerHTML = output;
+    element.hidden = false; // show output
+  }
 
   function getAllRawText(node) {
     let rawText = '';
@@ -80,7 +182,7 @@ const getAllChildren = (htmlElement) => { // get all children of a node
                 var p1 = container.parentNode; // for stuff inside spans
                 var p2 = p1.parentNode.parentNode; // for stuff outside spans
                 if(p1.nodeName.toLowerCase() === 'code' || p2.nodeName.toLowerCase() === 'pre'){
-                    // console.log("Code is valid!");
+                    // console.log("Got some code!");
                     rawText += node.textContent.trim() + (container.classList.contains('hljs-comment') ? '\n' : " ");
                 }
             }
@@ -105,19 +207,36 @@ function handleDOMChanges(mutationsList) {
                 try{
                     // Check if the added node is a ChatGPT response element
                     if (node.classList.contains('prose')) {
-                        console.log("Grabbed a new response");
-                        console.log("Waiting for GPT..."); // wait for chatgpt to finish printing. this is a lazy fix, i'm sure there's probably an actual event for it.
+                        // console.log("Grabbed a new response");
+                        console.log("GPTVulnScan: Waiting for GPT..."); // wait for chatgpt to finish printing. this is a lazy fix, i'm sure there's probably an actual event for it.
                         setTimeout(function sleep(){
                             // console.log("Grabbing text.");
                             var rawtext = getAllRawText(node);
                             var tokens = rawtext.split(" ");
                             if (tokens[0] === "javascript"){
-                                rawtext = rawtext.substring(9);
-                                console.log(rawtext);
+                                var rawtext = rawtext.substring(9);
+                                // console.log(rawtext);
+                                /**
+                                let grader = CodeGrader(rawtext, "JavaScript") // create a new grader
+                                grader.saveFile // save grader obj
+                                let graderOut = grader.grade // run through scanner
+                                */
                             }
+                            var exampleScore = rawtext.length%10; // just use an example score for now
                             results = document.createElement("div");
-                            results.className = "flex flex-col text-sm gradecontainer";
-                            results.innerHTML = "TEST"
+                            results.className = "flex flex-col text-sm";
+                            results.style = " \
+                                display: flex; \
+                                flex-direction: row; \
+                                width: 100%; \
+                                min-height:100px; \
+                                height:auto; \
+                                padding: 10px; \
+                                border-radius: 10px; \
+                                margin: 0 auto; \
+                                font-family: Arial, Helvetica, sans-serif; \
+                            ";
+                            displayGrade(results,exampleScore);
                             // node.parentNode.insertBefore(results, node.nextSibling);
                             node.appendChild(results);
                         }, 10000);
